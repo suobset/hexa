8bfddd9e890e348d388914290da566e2c985f611
Kushagra Srivastava <kushagra2410@gmail.com>
2026-01-28 12:51:25 -0500

    Initial Commit

---
+++ hexa/hexaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hexaApp.swift
+//  hexa
+//
+//  Created by Kushagra Srivastava on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct hexaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
